d157fd2a3721e2bccebe3a9533e129b55970f425
51703227 <51703227@student.tdtu.edu.vn>
2021-05-07 00:03:22 +0700

    Switch SQLite to MySQL

--- check_price/settings.py
+++ check_price/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'check_price.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'db',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
