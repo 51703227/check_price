5e65ff883fcf9bb50f5f40ef3df9532ee01579f0
Phuoc <nguyenhuuphuoc1806@gmail.com>
2021-06-11 01:29:04 +0700

    update scrapy and data from xtmobile website

--- scraper/spiders/mobile_dienthoaimoi_spider.py
+++ scraper/spiders/mobile_dienthoaimoi_spider.py
@@ -87,3 +87,64 @@ class CellPhonesMobileSpider(scrapy.Spider):
         item['thuoctinh'] = attributes
 
         return item
+
+
+#crawl xtmobile website
+class xtmobile_spider(scrapy.Spider):
+    name = 'mobile_xttmobile'
+    start_urls = ['https://www.xtmobile.vn/dien-thoai', ]
+
+    def parse(self, response):
+        self.log('Visited ' + response.url)
+
+        products = response.css('div.col-main-base-product > div.list_product_base > div.product-base-grid')
+
+        self.log('products ' + str(len(products)))
+        for product in products:
+
+            title = name_processing(product.css('div.boxItem > div.pinfo > h3 > a::text').get())
+            item_link = product.css('div.boxItem > div.pic > a::attr(href)').get()
+            thuong_hieu = item_link.split('/')[3].split('-')[0]
+            image1 = product.css('div.boxItem > div.pic a > img::attr(src)').get()
+
+            item = {
+                'ten': title,
+                'url': item_link,
+                'image': image1,
+                'ngay': date.today().strftime("%Y/%m/%d"),
+                'loaisanpham': 'dienthoai',
+                'thuonghieu': thuong_hieu
+            }
+            yield scrapy.Request(url=item_link, meta={'item': item}, callback=self.get_detail)
+
+
+        next_page_url = response.css('div.pagination-more > form > a.data-url::attr(href)').extract_first()
+        next_page_url = response.urljoin(next_page_url)
+        yield scrapy.Request(url=next_page_url, callback=self.parse)
+
+    def get_detail(self, response):
+        self.log('Visited ' + response.url)
+        item = response.meta['item']
+
+        option_old_price = response.css('form#form_order > div.product-color > ul.color-list-show > li > b::text').get()
+        option_rom = response.css('#parameter > div.option_focus > ul > li:nth-child(6) > strong::text').get()
+
+        option_color = response.css('form#form_order > div.product-color > ul.color-list-show > li > p::text').get()
+        option_new_price = option_old_price
+        active = True
+
+        # image2 = response.css('div.frame_img > div > div.magic_zoom_area > a::attr(href)').get()
+
+        attributes = []
+
+        attributes.append({
+            'bonho': option_rom,
+            'mausac': option_color,
+            'giagoc': option_old_price,
+            'giamoi': option_new_price,
+            'active': active
+        })
+
+        item['thuoctinh'] = attributes
+
+        return item
