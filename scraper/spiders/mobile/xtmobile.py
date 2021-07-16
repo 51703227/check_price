755a7e602809426752611fd5ffe1d63e5801d0fc
Phuoc <nguyenhuuphuoc1806@gmail.com>
2021-07-17 00:42:36 +0700

    update

--- scraper/spiders/mobile/xtmobile.py
+++ scraper/spiders/mobile/xtmobile.py
@@ -6,26 +6,66 @@ basedir = os.path.dirname(os.path.realpath('__file__'))
 
 # Xử lý tên sản phẩm
 # Danh sách các từ cần loại bỏ
-black_list = ['Chính', 'hãng', 'I', 'VN/A', 'chính','128GB','64GB','256GB','(','Fan', 'Edition',')',
-              '4GB','8GB','3GB','-','32GB','6GB','Ram','2GB','5GB','(2021)','(6GB','128GB)',
-              '(Đã', 'kích', 'hoạt','hành)','Điện','thoại','2018',
-              'Mi','Festival)','(Fan','Edition)','Mỹ', '(','Chưa', 'Active', ')','Mới',
-             '100%','Nguyên','Seal','mới','nguyên','seal','chưa','active','Full', 'VAT','4G','Wifi',
-              '-', 'Mới', '100%', 'Đã', 'kích','hoạt','ATO-','ATO', 'Quà', 'tặng', '2', 'triệu', 'đồng',
-              'Hãng', '(VN/A)','100%-', 'Kích', 'Hoạt','cũ' ,'Quốc' ,'tế','quốc','Pin','pin','Tế','512GB'
-              ,'siêu', 'lướt','6GB/128GB','DGW','8GB/128GB','8GB/256GB','(4G)','6GB/64GB','6GB/128GB',
-              '3GB/64GB','4GB/128GB','(8GB/128GB)','(6GB/128GB)','(6GB/64GB)','3GB/32GB','2GB/32GB','4GB/64GB',
-              '(8GB','siêu', 'lướt','LL/A','bản','16GB','(4G)', ' đẹp', 'như',
-              '12GB/256GB','(Plus)','(8Gb/256Gb)','/','Đẹp','12Gb/512Gb']
-
-# Hàm xử lý tên sản phẩm
 def name_processing(name):
-  unprocess_name = name.split()
-  processed_name = []
-  for i in unprocess_name:
-    if i not in black_list:
-      processed_name.append(i)
-  return ' '.join(processed_name)
+    bl_list = ['(', ')', '-', '–', '/', '[', ']', ',',
+               'camera kép', 'camera', 'còn bảo hành', 'Single Sim', 'Không Có Google', 'Pin 100%', 'qt',
+               'bảo hành chính hãng', 'quốc tế mới 100%', 'mới 100%', 'nguyên seal', 'chưa active', 'Quốc tế', '95%',
+               'QT', '100%', 'Chưa Active', '2017', 'Sạc Ít Lần', 'Fullbox', 'Phiên',
+               'Bản', 'Mới 100%', 'Quốc Tế', 'Hàn Quốc', 'Bản Hàn Quốc', 'tím', 'màu', 'Đẹp', 'đẹp', 'Deep Gray',
+               'Máy Người Già', 'Máy người già', 'Mùa Xuân',
+               'mùa hè', 'huyền bí', 'Đồng ánh kim', 'Ánh Kim', 'Đen Than', 'Ánh Sao', 'Thiên',
+               'Màu', 'Mới', 'mới', 'Rom',
+               '+512GB', '+256GB', '+128GB', '+64GB', '+8GB', '+16GB', '+32GB', '+4GB', '+512Gb', '+256Gb', '+128Gb',
+               '+64Gb', '+8Gb', '+16Gb', '+32Gb', '+4Gb',
+               '512GB', '256GB', '128GB', '64GB', '8GB', '16GB', '32GB', '4GB', '512Gb', '256Gb', '128Gb', '64Gb',
+               '8Gb', '16Gb', '32Gb', '4Gb',
+               '512 GB', '256 GB', '128 GB', '64 GB', '8 GB', '16 GB', '32 GB', '4 GB', '+512G', '+256G', '+128G',
+               '+64G', '+16G', '+32G',
+               '512gb', '256gb', '128gb', '64gb', '8gb', '16gb', '32gb', '6gb', '4gb', '512G', '256G', '128G', '64G',
+               '16G', '32G', '2GB', '3GB', '512g', '256g', '128g', '64g', '8g', '16g', '32g', '4g', '6g', '3g', '5g',
+               'Xanh đại dương', 'đại dương', 'ánh sao', 'hoàng hôn', 'Xanh lá', 'Vàng đồng', 'nước biển',
+               'Vàng đồng', 'lá', 'lục', 'Đồng', 'Khói', 'bích', 'huyền bí', 'nhật thực', 'Biển', 'mận', 'Dương',
+               'Lá', 'Đỏ', 'Đen', 'Lục', 'Cực', 'Quang', 'tinh', 'thạch', 'Ngọc', 'Trai', 'Bạc', 'Hà', 'Lam', 'Thủy',
+               'Triều', 'Đồng', 'Vàng', 'Xanh', 'Đen', 'Trắng', 'Thạch', 'Anh', 'lá', 'ngọc', 'lam', 'Sapphire',
+               'Pink', 'Deep Gray', 'Deep', 'Mint', 'Yellow', 'Champagne', 'Grey', 'Black', 'Gold', 'Graphite',
+               'Silver', 'Blue', 'Tím', 'Green', 'Sliver', 'Trắng', 'Xám', 'Pacific', 'Blue', 'White', 'Gray',
+               'Violet', 'Purple', 'Red', 'Browns',
+               'bảo hành', 'độc', 'đáo', 'hạt', 'tiêu', '(KHÔNG KÈM THẺ NHỚ)', 'Thoại', '2019', '2020', '2018', '2017',
+               'Bộ 3 Camera', 'Bộ 4 Camera Sau 48Mp', 'Siêu Màn Hình', 'Màn Hình Giọt Nước', 'Sạc Nhanh',
+               '6.67Inch', '6.5Inch', '6.9Inch', '2 sim', '6.1Inch', '2 Sim', 'VNA', 'hải', 'quân', 'san hô', 'trai',
+               'dương', 'cẩm', 'KHÔNG KÈM THẺ NHỚ', 'San Hô', 'Nhật Thực', 'Sương Mai', 'Đam Mê', 'lục', 'bảo', 'Bảo',
+               'sương', 'hồng', 'Bích', 'tú', 'thủy', 'Hải', 'Âu', 'Hồng', 'pha', 'lê', 'quang', 'cực', 'Cam', 'hà',
+               'Phong', 'Vân',
+               '1 sim', '1 Sim', 'Mỹ', 'New', 'BH12T', 'Certified', 'PreOwned', 'Special', 'Product', 'ram', 'cty',
+               'RAM', 'Edge', 'Batman', 'Injustice', 'Cty',
+               'Apple', 'APPLE', '6.4Inch', '5.3Inch', '6.4Inch', '6.23Inch', '6.2Inch', '5.7Inch', '6.2Inch',
+               '6.4Inch', 'Đại', 'like',
+               'Đtdđ', 'ĐTDĐ', '8+128', '6 + 128',
+               '2+32', '9798%', '98%', '97%', '99%', '95%', 'Camera Sau', 'Active | Pin 4000Ma, Chip Snap 835',
+               'Pin 5000Mah', 'Pin 5000 Mah', 'Không Có Google', 'Fan Edition', 'Siêu Lướt', 'Đã Kích Hoạt',
+               ' \t', '\t', '\r', '\n', 'Chính Hãng', '  ', 'chính Hãng', 'Chính hãng', 'chính hãng',
+               'Cũ', 'Ato', 'Dgw', 'Như', 'Nh 12 Tháng 1 Đổi 1', 'Lte', 'Lla',
+               '   ',
+               ]
+
+    if name == None:
+        return ''
+    for character in bl_list:
+        name = name.replace(character, '')
+        name = name.replace(character.lower(), '')
+        name = name.replace(character.title(), '')
+        name = name.replace(character.upper(), '')
+
+    black_list1 = ['Siêu', 'Lướt', 'siêu', 'lướt', 'Xlnoboxtbh', 'V405Qa7', '101', 'V409N',
+                   'xlnoboxtbh', 'v405Qa7', '101', 'v409N', 'nobox', 'tbh',
+                   'Nobox', 'Tbh', 'V405', 'Qa7']
+
+    unprocess_name = name.split()
+    processed_name = []
+    for i in unprocess_name:
+        if i not in black_list1:
+            processed_name.append(i)
+    return ' '.join(processed_name).title()
 
 #Xử lý price
 def format_price(price):
@@ -61,7 +101,9 @@ class xtmobile_spider(scrapy.Spider):
         self.log('products ' + str(len(products)))
         for product in products:
 
-            title = name_processing(product.css('div.boxItem > div.pinfo > h3 > a::text').get())
+            title1 = name_processing(product.css('div.boxItem > div.pinfo > h3 > a::text').get())
+            title = title1.split('|')[0]
+
             item_link = product.css('div.boxItem > div.pic > a::attr(href)').get()
             thuong_hieu = item_link.split('/')[3].split('-')[0]
             image1 = product.css('div.boxItem > div.pic a > img::attr(src)').get()
